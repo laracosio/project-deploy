6a357dbf02c2d0654090d92d409fd93a32b7b87a
Lara Michelle Cosio <z5403305@ad.unsw.edu.au>
2023-09-20 11:08:06 +1000

    Created the adminQuizInfo stub function for Iteration 0

---
+++ src/quiz.js
@@ -0,0 +1,18 @@
+//Stub function for adminQuizInfo by Lara
+
+/**
+ * 
+ * @param {number} authUserId 
+ * @param {number} quizId 
+ * @returns {quizId: number, name: string, timeCreated: number, timeLastEdited: number, description: string}
+ */
+
+function adminQuizInfo (authUserId, quizId) {
+    return {
+        quizId: 1,
+        name: 'Interation 0 Quiz',
+        timeCreated: 0920,
+        timeLastEdited: 0921,
+        description: 'This is my quiz for Iteration 0',
+      }
+}
