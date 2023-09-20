a1535010280f4bfc6b5086f5f8c43f44fec2d48b
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-09-20 11:32:59 +1000

    created stub function for adminAuthLogin

---
+++ src/auth.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @param {number} email - unique email address
+ * @param {number} password - user's password
+ * @return {{authUserId: number}} on successful log in
+ * @returns {{error: string}} on error
+ */
+
+function adminAuthLogin(email, password) {
+    return {
+        authUserId: 1, 
+    }
+}
