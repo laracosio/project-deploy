5c4da40f4067dc6030446888f5849e9d63c09ff0
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-09-20 11:23:53 +1000

    created stub function for adminAuthRegister

---
+++ src/auth.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @param {string} email - unique email address
+ * @param {string} password - password of user's choice
+ * @param {string} nameFirst - user's first name
+ * @param {string} nameLast - user's last name
+ * @returns {{authUserId: number}}
+ * @returns {{error: string}} on error
+ */
+function adminAuthRegister(email, password, nameFirst, nameLast) {
+    return {
+        authUserId: 1,
+    };
+}
+
