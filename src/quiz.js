cba3ca424d4ef5ceeb409624129035183d8a00a7
Gina Kong <ginagkong@gmail.com>
2023-09-20 11:07:59 +1000

    Added adminQuizCreate function stub to quiz.js

---
+++ src/quiz.js
@@ -0,0 +1,14 @@
+
+/**
+ * @param {number} authUserId - unique identifier for authorised user
+ * @param {string} name - of quiz
+ * @param {string} description - of quiz
+ * @returns {quizId: 2}
+ */
+
+function adminQuizCreate(authUserId, name, description) {
+    return {
+        quizId: 2,
+    }
+}
+
