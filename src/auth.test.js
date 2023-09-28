3eafb0f61bedc86b73e7c93a3e6d3929593be167
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-09-28 23:24:38 +1000

    Update tests to use testingData

--- src/auth.test.js
+++ src/auth.test.js
@@ -19,53 +19,57 @@ beforeEach(() => {
 // tests for adminAuthRegister
 describe('Success cases - adminAuthRegister', () => {
   test('1 user', () => {
-    expect(adminAuthRegister('testEmail@unsw.edu.au', 'password123', 'John', 'Smith').toMatchObject({ authUserId: 1 }));
+    expect(adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast).toMatchObject({ authUserId: 1 }));
   })
   test('2 users', () => {
-    adminAuthRegister('testEmail1@unsw.edu.au', 'password123', 'John', 'Smith');
-    expect(adminAuthRegister('testEmail2@unsw.edu.au', 'password456', 'Jane', 'Doe').toMatchObject({ authUserId: 2}));
+    adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
+    expect(adminAuthRegister(person2.email, person2.password, person2.nameFirst, person2.nameLast).toMatchObject({ authUserId: 2}));
   })
-  test('3 users', () => {
-    adminAuthRegister('testEmail2@unsw.edu.au', 'password123', 'John', 'Smith');
-    adminAuthRegister('testEmail2@unsw.edu.au', 'password456', 'Jane', 'Doe');
-    expect(adminAuthRegister('testEmail3@unsw.edu.au', 'password789', 'Pumpkin', 'Gizmo').toMatchObject({ authUserId: 3}));
+  test('7 users', () => {
+        adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
+		adminAuthRegister(person2.email, person2.password, person2.nameFirst, person2.nameLast);
+		adminAuthRegister(person3.email, person3.password, person3.nameFirst, person3.nameLast);
+		adminAuthRegister(person4.email, person4.password, person4.nameFirst, person4.nameLast);
+		adminAuthRegister(person5.email, person5.password, person5.nameFirst, person5.nameLast);
+		adminAuthRegister(person6.email, person6.password, person6.nameFirst, person6.nameLast);
+    expect(adminAuthRegister(person7.email, person7.password, person7.nameFirst, person7.nameLast).toMatchObject({ authUserId: 7}));
   })
 })
 
 describe('Error cases - adminAuthRegister', () => {
   test('email address - duplicate', () => {
-    adminAuthRegister('testEmail@unsw.edu.au', 'password123', 'John', 'Smith');
-    expect(adminAuthRegister('testEmail@unsw.edu.au', 'password456', 'Jane', 'Doe').toStrictEqual({ ERROR }));
+    adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
+    expect(adminAuthRegister(person1.email, person2.password, person2.nameFirst, person2.nameLast).toStrictEqual({ ERROR }));
   })
   test('email address - invalid', () => {
-    expect(adminAuthRegister('testEmail@@unsw.edu.au', 'password456', 'Jane', 'Doe').toStrictEqual({ ERROR }));
+    expect(adminAuthRegister('h.simpson@@springfield.com', person1.password, person1.nameFirst, person1.nameLast).toStrictEqual({ ERROR }));
   })
   test('first name - invalid characters', () => {
-    expect(adminAuthRegister('testEmail@@unsw.edu.au', 'password456', '123', 'Doe').toStrictEqual({ ERROR }));
+    expect(adminAuthRegister(person1.email, person1.password, 'H0mer', person1.nameLast).toStrictEqual({ ERROR }));
   })
   test('first name - too short', () => {
-    expect(adminAuthRegister('testEmail@@unsw.edu.au', 'password456', 'J', 'Doe').toStrictEqual({ ERROR }));
+    expect(adminAuthRegister(person1.email, person1.password, 'H', person1.nameLast).toStrictEqual({ ERROR }));
   })
   test('first name - too long', () => {
-    expect(adminAuthRegister('testEmail@@unsw.edu.au', 'password456', 'JaneDoeDoeDoeDoeDoeDoe', 'Doe').toStrictEqual({ ERROR }));
+    expect(adminAuthRegister(person1.email, person1.password, 'HomerHasAVeryLongName', person1.nameLast).toStrictEqual({ ERROR }));
   })
   test('last name - invalid characters', () => {
-    expect(adminAuthRegister('testEmail@unsw.edu.au', 'password456', 'Jane', '123').toStrictEqual({ ERROR }));
+    expect(adminAuthRegister(person1.email, person1.password, person1.nameFirst, '$!MP$0N').toStrictEqual({ ERROR }));
   })
   test('last name - too short', () => {
-    expect(adminAuthRegister('testEmail@unsw.edu.au', 'password456', 'Jane', 'D').toStrictEqual({ ERROR }));
+    expect(adminAuthRegister(person1.email, person1.password, person1.nameFirst, 'S').toStrictEqual({ ERROR }));
   })
   test('last name - too long', () => {
-    expect(adminAuthRegister('testEmail@unsw.edu.au', 'password456', 'Jane', 'DoeDoeDoeDoeDoeDoeDoe').toStrictEqual({ ERROR }));
+    expect(adminAuthRegister(person1.email, person1.password, person1.nameFirst, 'SimpsonSimpsonSimpson').toStrictEqual({ ERROR }));
   })
   test('password - too short', () => {
-    expect(adminAuthRegister('testEmail@unsw.edu.au', '123567', 'Jane', 'Doe').toStrictEqual({ ERROR }));
+    expect(adminAuthRegister(person1.email, 'a1', person1.nameFirst, person1.nameLast).toStrictEqual({ ERROR }));
   })
   test('password - all letters', () => {
-    expect(adminAuthRegister('testEmail@unsw.edu.au', 'ABCDEFGH', 'Jane', 'Doe').toStrictEqual({ ERROR }));
+    expect(adminAuthRegister(person1.email, 'ABCDEFG', person1.nameFirst, person1.nameLast).toStrictEqual({ ERROR }));
   })
   test('password - all numbers', () => {
-    expect(adminAuthRegister('testEmail@unsw.edu.au', '1235678', 'Jane', 'Doe').toStrictEqual({ ERROR }));
+    expect(adminAuthRegister(person1.email, '1234567', person1.nameFirst, person1.nameLast).toStrictEqual({ ERROR }));
   })
 })
 
