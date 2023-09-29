5eafca3ea6bb58aa4fa19f29caae1f5cc7c47673
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-09-29 16:05:11 +1000

    added additional test for multiple users and multiple tests

--- src/clear.test.js
+++ src/clear.test.js
@@ -11,7 +11,14 @@ describe('Success cases - clear', () => {
     })
     test('clear - user and quiz', () => {
         adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
-        adminQuizCreate(1, 'John Smith', 'Misc Description');
+        adminQuizCreate(1, 'Misc Quiz Name', 'Misc Description');
+        expect(clear().toBe({}));
+    })
+    test('clear - multiple users and quizzes', () => {
+        adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
+        adminAuthRegister(person2.email, person2.password, person2.nameFirst, person2.nameLast);
+        adminQuizCreate(1, 'Misc Quiz Name', 'Misc Description');
+        adminQuizCreate(2, 'Misc Quiz Name2', 'Misc Description2');
         expect(clear().toBe({}));
     })
 })
