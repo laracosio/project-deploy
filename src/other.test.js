e3eea864a95c07045338bb5ea7253b61516a0081
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-10-02 09:50:19 +1100

    updated jest matcher for tests which include quiz functions (cannot be tested yet)

--- src/other.test.js
+++ src/other.test.js
@@ -12,13 +12,13 @@ describe('clear - Success Cases', () => {
     // test('clear - user and quiz', () => {
     //     adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
     //     adminQuizCreate(1, 'Misc Quiz Name', 'Misc Description');
-    //     expect(clear().toBe({}));
+    //     expect(clear().toStrictEqual({});
     // })
     // test('clear - multiple users and quizzes', () => {
     //     adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
     //     adminAuthRegister(person2.email, person2.password, person2.nameFirst, person2.nameLast);
     //     adminQuizCreate(1, 'Misc Quiz Name', 'Misc Description');
     //     adminQuizCreate(2, 'Misc Quiz Name2', 'Misc Description2');
-    //     expect(clear().toBe({}));
+    //     expect(clear().toStrictEqual({});
     // })
 })
