c18441d003f510c9b903bf26bf8fd13874c9a88b
Lara Michelle Cosio <z5403305@ad.unsw.edu.au>
2023-10-02 16:57:17 +1100

    Fixed testing for authLogin

--- src/auth.test.js
+++ src/auth.test.js
@@ -6,7 +6,7 @@
 TO DO!! 
 'auth.js' or auth.js ??? 
 */
-import { adminAuthLogin } from 'auth.js';
+import { adminAuthRegister, adminAuthLogin } from 'auth.js';
 import { person1, person2, person3, person4, person5, person6, person7 } from '.testingData.js';
 import { clear } from '.other.js'
 
@@ -27,45 +27,28 @@ import { clear } from '.other.js'
 // tests for adminAuthLogin
 
 beforeEach(() => {
-  setData({
-    users: [
-      {
-        email: 'comp1531@gmail.com',
-        password: 'Aero321',
-        nameFirst: 'Lara',
-        nameLast: 'Cosio',
-        authUserId: 1,
-        numSuccessfulLogins: 1,
-        numFailedPasswordsSinceLastLogin: 0,
-      },
-      {
-        email: 'aero1531@gmail.com',
-        password: 'Comp321',
-        nameFirst: 'Carmen',
-        nameLast: 'Zhang',
-        authUserId: 2,
-        numSuccessfulLogins: 1,
-        numFailedPasswordsSinceLastLogin: 0,
-      }
-    ] 
-  });
+
+ clear();
 
 });
 
-afterAll(() => {
-  setData({});
-});
 
 describe('Testing adminAuthLogin', () => {
   test('Return authUserId if email and password are both correct', () => {
-    expect({ authUserId: adminAuthLogin('comp1531@gmail.com', 'Aero321')}).toStrictEqual({ authUserId: 1 });
-    expect({ authUserId: adminAuthLogin('aero1531@gmail.com', 'Comp321')}).toStrictEqual({ authUserId: 2 });
+    let user1 = adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
+    let user2 = adminAuthRegister(person2.email, person2.password, person2.nameFirst, person2.nameLast);
+    let user3 = adminAuthRegister(person3.email, person3.password, person3.nameFirst, person3.nameLast);
+    let user4 = adminAuthRegister(person4.email, person4.password, person4.nameFirst, person4.nameLast);
+    expect(adminAuthLogin(person1.email, person1.password)).toStrictEqual(user1);
+    expect(adminAuthLogin(person2.email, person2.password)).toStrictEqual(user2);
+    expect(adminAuthLogin(person3.email, person3.password)).toStrictEqual(user3);
+    expect(adminAuthLogin(person4.email, person4.password)).toStrictEqual(user4);
   });
   test('Return error when email does not belong to a user', () => {
-    expect({authUserId: adminAuthLogin('invalidemail@@com', 'asdfghjkl')}).toStrictEqual({ error: expect.any(String)});
+    expect(adminAuthLogin(person5.email, person5.password)).toStrictEqual({ error: expect.any(String)});
   });
   test('Return error when password is not correct', () => {
-    expect(adminAuthLogin('comp1531@gmail.com', 'Boost21')).toStrictEqual({ error: expect.any(String)});
+    expect(adminAuthLogin(person1.email, person4.password)).toStrictEqual({ error: expect.any(String)});
   });
 });
 
