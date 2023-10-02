9ce2c821e5387380ce6816cb5fdd6767bd29d83e
Gina Kong <ginagkong@gmail.com>
2023-10-02 17:14:59 +1100

    Fixed tests for adminQuizCreate

--- src/adminQuizCreate.test.js
+++ src/adminQuizCreate.test.js
@@ -1,50 +1,52 @@
 // function imports
 import { 
-    validQuizName,
-    invalidQuizName,
-    shortQuizName,
-    longQuizName,
-    validQuizDescription,
-    longQuizDescription, 
+	person1,
+	validQuizName,
+	invalidQuizName,
+	shortQuizName,
+	longQuizName,
+	validQuizDescription,
+	longQuizDescription, 
 } from './testingData.js';
 import { adminQuizCreate} from "./quiz";
 import { adminAuthRegister } from "./auth";
 import { clear } from "./other";
 
 // test reset and test data
+let validUserId;
+
 beforeEach(() => {
-    clear();
-    const validuserId = adminAuthRegister('gina@unsw.edu.au', 'abcde12345!', 'Gina', 'Kong');
+	clear();
+	validUserId = adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
 });
 
 // adminQuizCreate tests
 describe('adminQuizCreate - Error Cases', () => {
-    test('invalid authUserId', () => {
-        expect(adminQuizCreate(validUserId.authUserId + 1, validQuizName, validQuizDescription)).toStrictEqual({ ERROR });
-    });
-    test('invalid name characters', () => {
-        expect(adminQuizCreate(validuserId.authUserId, invalidQuizName, validQuizDescription)).toStrictEqual({ ERROR });
-    });
-    test('invalid name length too short', () => {
-        expect(adminQuizCreate(validuserId.authUserId, shortQuizName, validQuizDescription)).toStrictEqual({ ERROR });
-    });
-    test('invalid name length too long', () => {
-        expect(adminQuizCreate(validuserId.authUserId, longQuizName, validQuizDescription)).toStrictEqual({ ERROR });
-    });
-    test('existing name', () => {
-        adminQuizCreate(validuserId.authUserId, validQuizName, validQuizDescription);
-        const invalidSecondQuizName = validQuizName;
-        expect(adminQuizCreate(validuserId.authUserId, invalidSecondQuizName, validQuizDescription)).toStrictEqual({ ERROR });
-    });
-    test('invalid description length', () => {
-        expect(adminQuizCreate(validuserId.authUserId, validQuizName, longQuizDescription)).toStrictEqual({ ERROR });
-    });
+	test('invalid authUserId', () => {
+		expect(adminQuizCreate(validUserId.authUserId + 1, validQuizName, validQuizDescription).error).toStrictEqual('Invalid user');
+	});
+	test('invalid name characters', () => {
+		expect(adminQuizCreate(validUserId.authUserId, invalidQuizName, validQuizDescription).error).toStrictEqual('Invalid name, must not contain special characters');
+	});
+	test('invalid name length too short', () => {
+		expect(adminQuizCreate(validUserId.authUserId, shortQuizName, validQuizDescription).error).toStrictEqual('Invalid name length');
+	});
+	test('invalid name length too long', () => {
+		expect(adminQuizCreate(validUserId.authUserId, longQuizName, validQuizDescription).error).toStrictEqual('Invalid name length');
+	});
+	test('existing name', () => {
+		adminQuizCreate(validUserId.authUserId, validQuizName, validQuizDescription);
+		const invalidSecondQuizName = validQuizName;
+		expect(adminQuizCreate(validUserId.authUserId, invalidSecondQuizName, validQuizDescription).error).toStrictEqual('Quiz name already in use');
+	});
+	test('invalid description length', () => {
+		expect(adminQuizCreate(validUserId.authUserId, validQuizName, longQuizDescription).error).toStrictEqual('Description must be less than 100 characters');
+	});
 });
 
 describe('adminQuizCreate - Passed Cases', () => {
-    test('valid quiz details', () => {
-        expect(adminQuizCreate(validuserId.authUserId, validQuizName, validQuizDescription)).toBe({
-            quizId: expect.any(Number),
-          })
-    })
+	test('valid quiz details', () => {
+		const quizCreate = adminQuizCreate(validUserId.authUserId, validQuizName, validQuizDescription)
+		expect(quizCreate.quizId).toEqual(expect.any(Number))
+	})
 })
