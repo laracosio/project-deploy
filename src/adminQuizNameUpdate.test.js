569edd2b204ca10546fba4ffac9214f51873d742
Joshua Beltran <z5488271@ad.unsw.edu.au>
2023-10-02 11:25:01 +1100

    added tests for adminQuizNameUpdate

---
+++ src/adminQuizNameUpdate.test.js
@@ -0,0 +1,22 @@
+// functions to import
+//e.g. import { clear, movieAdd, movieEdit, moviesList } from './movie';
+
+
+// Any test resets
+
+
+/** test template
+ * describe('testGroupName', () => {
+ *  test('nameOfIndividualTest', () => {
+ *   TESTCODE_GOES_HERE
+ *  expect(WHAT_YOU_EXPECT_TO_BE_RETURNED).equalityParameter(equalityField);
+ * })
+ */
+
+// tests for adminAuthRegister
+
+
+// tests for adminAuthLogin
+
+
+// tests for adminUserDetails
