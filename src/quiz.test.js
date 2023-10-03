2391e1d00e92d052b8355d7b55ddcf470b371884
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-10-03 19:27:11 +1100

    added additional tests to ensure that multiple quizzes can be removed

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -23,6 +23,22 @@ describe('adminQuizRemove - Success Cases', () => {
     const quiz2 = adminQuizCreate(person.authUserId, 'Misc Quiz Name2', 'Misc Quiz Description2');
     expect(adminQuizRemove(person.authUserId, quiz2.quizId)).toStrictEqual({});
   })
+  test('multiple quizzes create, multiple quizzes removed', () => {
+    const person = adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
+    const personB = adminAuthRegister(person2.email, person2.password, person2.nameFirst, person2.nameLast);
+    const quiz = adminQuizCreate(person.authUserId, 'Quiz1', 'Misc Quiz Description');
+    const quiz2 = adminQuizCreate(person.authUserId, 'Quiz2', 'Misc Quiz Description2');
+    const quiz3 = adminQuizCreate(person.authUserId, 'Quiz3', 'Misc Quiz Description3');
+    const quiz4 = adminQuizCreate(person.authUserId, 'Quiz4', 'Misc Quiz Description4');
+    const quiz5 = adminQuizCreate(personB.authUserId, 'Quiz5', 'Misc Quiz Description5');
+    const quiz6 = adminQuizCreate(personB.authUserId, 'Quiz6', 'Misc Quiz Description6');
+    const quiz7 = adminQuizCreate(personB.authUserId, 'Quiz7', 'Misc Quiz Description7');
+    const quiz8 = adminQuizCreate(personB.authUserId, 'Quiz8', 'Misc Quiz Description8');
+    expect(adminQuizRemove(personB.authUserId, quiz7.quizId)).toStrictEqual({});
+    expect(adminQuizRemove(person.authUserId, quiz4.quizId)).toStrictEqual({});
+    expect(adminQuizRemove(personB.authUserId, quiz5.quizId)).toStrictEqual({});
+    expect(adminQuizRemove(person.authUserId, quiz2.quizId)).toStrictEqual({});
+  })
 })
 
 describe('adminQuizRemove - Error Cases', () => {
