a65d08bf28467d3de58a9d107389eb0e80833ccc
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-10-03 17:23:35 +1100

    updated spacing and uncommented quizCreate tests

--- src/other.test.js
+++ src/other.test.js
@@ -1,24 +1,24 @@
 import { adminAuthRegister } from './auth.js';
-//import { adminQuizCreate} from 'quiz.js';
+import { adminQuizCreate} from './quiz.js';
 import { person1, person2 } from './testingData.js';
 import { clear } from './other.js';
 
 describe('clear - Success Cases', () => {
-    test('clear - user', () => {
-        adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
-        adminAuthRegister(person2.email, person2.password, person2.nameFirst, person2.nameLast);
-        expect(clear()).toStrictEqual({});
-    })
-    // test('clear - user and quiz', () => {
-    //     adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
-    //     adminQuizCreate(1, 'Misc Quiz Name', 'Misc Description');
-    //     expect(clear().toStrictEqual({});
-    // })
-    // test('clear - multiple users and quizzes', () => {
-    //     adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
-    //     adminAuthRegister(person2.email, person2.password, person2.nameFirst, person2.nameLast);
-    //     adminQuizCreate(1, 'Misc Quiz Name', 'Misc Description');
-    //     adminQuizCreate(2, 'Misc Quiz Name2', 'Misc Description2');
-    //     expect(clear().toStrictEqual({});
-    // })
+  test('clear - user', () => {
+    adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
+    adminAuthRegister(person2.email, person2.password, person2.nameFirst, person2.nameLast);
+    expect(clear()).toStrictEqual({});
+  })
+  test('clear - user and quiz', () => {
+    adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
+    adminQuizCreate(1, 'Misc Quiz Name', 'Misc Description');
+    expect(clear()).toStrictEqual({});
+  })
+  test('clear - multiple users and quizzes', () => {
+    adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
+    adminAuthRegister(person2.email, person2.password, person2.nameFirst, person2.nameLast);
+    adminQuizCreate(1, 'Misc Quiz Name', 'Misc Description');
+    adminQuizCreate(2, 'Misc Quiz Name2', 'Misc Description2');
+    expect(clear()).toStrictEqual({});
+  })
 })
