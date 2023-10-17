189399c2ef20b3692efd6fa9f585d73e4bc39791
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-10-17 20:38:31 +1100

    updated clear testing

--- src/other.test.js
+++ src/other.test.js
@@ -12,16 +12,16 @@ describe('clear - Success Cases', () => {
     expect(clear()).toStrictEqual({});
   });
   test('clear - user and quiz', () => {
-    const user1 = adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
-    const quiz1 = adminQuizCreate(user1.authUserId, 'Misc Quiz Name', 'Misc Description');
+    const session = adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
+    const quiz1 = adminQuizCreate(session.token, 'Misc Quiz Name', 'Misc Description');
     expect(clear()).toStrictEqual({});
-    expect(adminQuizRemove(user1.authUserId, quiz1.quizId)).toEqual(ERROR);
+    expect(adminQuizRemove(session.token, quiz1.quizId)).toEqual(ERROR);
   });
   test('clear - multiple users and quizzes', () => {
-    const user1 = adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
+    const session = adminAuthRegister(person1.email, person1.password, person1.nameFirst, person1.nameLast);
     adminAuthRegister(person2.email, person2.password, person2.nameFirst, person2.nameLast);
-    adminQuizCreate(user1.authUserId, 'Misc Quiz Name', 'Misc Description');
-    adminQuizCreate(2, 'Misc Quiz Name2', 'Misc Description2');
+    adminQuizCreate(session.token, 'Misc Quiz Name', 'Misc Description');
+    adminQuizCreate(session.token, 'Misc Quiz Name2', 'Misc Description2');
     expect(clear()).toStrictEqual({});
     expect(adminAuthLogin(person1.email, person1.password)).toEqual(ERROR);
   });
