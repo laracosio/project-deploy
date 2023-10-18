e6d5d4d8eb0deef3661996c812abc1018613d7b2
Gina Kong <ginagkong@gmail.com>
2023-10-18 21:35:18 +1100

    Fixed linting

--- src/features/trash.ts
+++ src/features/trash.ts
@@ -11,33 +11,33 @@ import { getUnixTime } from 'date-fns';
  * @returns {{error: string}}
  */
 function adminQuizRemove(token:string, quizId: number): object {
-    const dataStore = getData();
-    // check that quizId is not empty or is valid
-    if (!quizId || !dataStore.quizzes.some(q => q.quizId === quizId)) {
-      throw new ApiError('Invalid quizId', HttpStatusCode.BAD_REQUEST);
-      // return { error: 'Invalid quizId' };
-    }
-  
-    // check that token is not empty or is valid
-    if (!tokenValidation(token)) {
-      throw new ApiError('Invalid token', HttpStatusCode.UNAUTHORISED);
-      // return { error: 'Invalid token' };
-    }
-  
-    // find user associated with token and checks whether they are the quiz owner
-    const tokenUser = findTokenUser(token);
-    if (dataStore.quizzes.some((q) => (q.quizOwner !== tokenUser.userId && q.quizId === quizId))) {
-      throw new ApiError('User does not own quiz to remove', HttpStatusCode.UNAUTHORISED);
-      // return { error: 'User does not own quiz to remove' };
-    }
-  
-    const quizIndex: number = dataStore.quizzes.findIndex(quiz => quiz.quizId === quizId);
-    const trashQuiz = dataStore.quizzes.splice(quizIndex, 1)[0];
-    trashQuiz.timeLastEdited = getUnixTime(new Date());
-    dataStore.trash.push(trashQuiz);
-  
-    setData(dataStore);
-    return {};
+  const dataStore = getData();
+  // check that quizId is not empty or is valid
+  if (!quizId || !dataStore.quizzes.some(q => q.quizId === quizId)) {
+    throw new ApiError('Invalid quizId', HttpStatusCode.BAD_REQUEST);
+    // return { error: 'Invalid quizId' };
   }
 
-  export { adminQuizRemove };
+  // check that token is not empty or is valid
+  if (!tokenValidation(token)) {
+    throw new ApiError('Invalid token', HttpStatusCode.UNAUTHORISED);
+    // return { error: 'Invalid token' };
+  }
+
+  // find user associated with token and checks whether they are the quiz owner
+  const tokenUser = findTokenUser(token);
+  if (dataStore.quizzes.some((q) => (q.quizOwner !== tokenUser.userId && q.quizId === quizId))) {
+    throw new ApiError('User does not own quiz to remove', HttpStatusCode.UNAUTHORISED);
+    // return { error: 'User does not own quiz to remove' };
+  }
+
+  const quizIndex: number = dataStore.quizzes.findIndex(quiz => quiz.quizId === quizId);
+  const trashQuiz = dataStore.quizzes.splice(quizIndex, 1)[0];
+  trashQuiz.timeLastEdited = getUnixTime(new Date());
+  dataStore.trash.push(trashQuiz);
+
+  setData(dataStore);
+  return {};
+}
+
+export { adminQuizRemove };
--- src/tests/trash.server.test.ts
+++ src/tests/trash.server.test.ts
@@ -1,5 +1,5 @@
 // // DELETE THIS LATER
 test('REMOVE ME - added to pass testing pipeline!', () => {
-    const number = 1;
-    expect(number).toBeGreaterThanOrEqual(1);
-  });
+  const number = 1;
+  expect(number).toBeGreaterThanOrEqual(1);
+});
