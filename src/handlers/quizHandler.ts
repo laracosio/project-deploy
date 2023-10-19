16a87fe148c8affde285421f1e7ee1208d369e63
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-10-19 22:45:43 +1100

    implemented quizRemove route

--- src/handlers/quizHandler.ts
+++ src/handlers/quizHandler.ts
@@ -1,6 +1,11 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+import { adminQuizRemove } from '../features/trash';
 
 export const quizRouter = Router();
 
-// quizRouter.post('/', (req: Request, res: Response) => {
-// });
+quizRouter.delete(':quizid', (req: Request, res: Response) => {
+    const sessionToken = req.query.token as string;
+    const quizId = parseInt(req.params.quizId);
+    const response = adminQuizRemove(sessionToken, quizId);
+    res.json(response);
+});
