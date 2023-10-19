f329b44d4fec1bba42e734ea32cf46e7cf012670
Gina Kong <ginagkong@gmail.com>
2023-10-19 13:22:29 +1100

    Fix: commented example use in handler files

--- src/handlers/quizHandler.ts
+++ src/handlers/quizHandler.ts
@@ -3,7 +3,5 @@ import { Request, Response, Router } from 'express';
 
 export const quizRouter = Router();
 
-quizRouter.post('/', (req: Request, res: Response) => {
-  const { token, name, description } = req.body;
-  res.send(adminQuizCreate(token, name, description));
-});
+// quizRouter.post('/', (req: Request, res: Response) => {
+// });
