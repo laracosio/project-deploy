7fd4992cc9f25ed9698173d551b64925b375ceda
Gina Kong <ginagkong@gmail.com>
2023-10-19 16:44:08 +1100

    Added authRegisterRequest to serverTestHelper.ts

---
+++ src/tests/serverTestHelper.ts
@@ -0,0 +1,13 @@
+import request from 'sync-request-curl';
+import { port, url } from '../config.json'
+
+const SERVER_URL = `${url}:${port}`;
+
+interface SessionReturn {
+  token: string
+}
+
+const requestSession = (email: string, password: string, nameFirst: string, nameLast: string): SessionReturn => {
+  const response = request('POST', `${SERVER_URL}/v1/auth/register`, { json: { email: email, password: password, nameFirst: nameFirst, nameLast: nameLast } });
+  return JSON.parse(response.body.toString());
+}
