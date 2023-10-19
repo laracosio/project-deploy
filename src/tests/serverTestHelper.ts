61f2bd6561cf5b9c0d463431a83c168c65f1e2ed
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-10-19 17:11:20 +1100

    added helper server function for clear, remove and create

--- src/tests/serverTestHelper.ts
+++ src/tests/serverTestHelper.ts
@@ -8,8 +8,47 @@ interface SessionReturn {
 }
 
 const authRegisterRequest = (email: string, password: string, nameFirst: string, nameLast: string): SessionReturn => {
-  const response = request('POST', `${SERVER_URL}/v1/auth/register`, { json: { email: email, password: password, nameFirst: nameFirst, nameLast: nameLast } });
+  const response = request('POST',
+    `${SERVER_URL}/v1/auth/register`, 
+    { json: { email: email, password: password, nameFirst: nameFirst, nameLast: nameLast } });
   return JSON.parse(response.body.toString());
 }
 
-export { authRegisterRequest, };
+const clearRequest = (): SessionReturn => {
+  const response = request(
+    'DELETE',
+    SERVER_URL + '/v1/clear'
+  );
+  return JSON.parse(response.body.toString());
+}
+
+const quizCreateRequest = (token: string, name: string, description: string): SessionReturn => {
+  const response = request(
+    'POST',
+    SERVER_URL + '/v1/admin/quiz',
+    {
+      body: JSON.stringify({
+        token: token,
+        name: name,
+        description: description
+      }),
+      headers: { 'Content-type': 'application/json' },
+    }
+  );
+  return JSON.parse(response.body.toString());
+}
+
+const quizRemoveRequest = (quizId: number, token: string) => {
+  const response = request(
+    'DELETE',
+    SERVER_URL + '/v1/admin//quiz/' + quizId,
+    {
+      qs: {
+        token: token
+      }
+    }
+  );
+  return JSON.parse(response.body.toString());
+}
+
+export { authRegisterRequest, clearRequest, quizRemoveRequest, quizCreateRequest };
