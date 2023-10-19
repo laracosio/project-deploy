4a7833ddcfadab7de025f45e409735fc846fa435
Gina Kong <ginagkong@gmail.com>
2023-10-19 11:41:02 +1100

    Fix: changed error message to return object

--- src/server.ts
+++ src/server.ts
@@ -56,7 +56,8 @@ app.get('/echo', (req: Request, res: Response) => {
 app.use((err: Error | ApiError, req: Request, res: Response, next: NextFunction) => {
   console.log(err);
   if (err instanceof ApiError) {
-    res.status(err.httpCode).json(err.message)
+
+    res.status(err.httpCode).json({ error: err.message} )
   } else {
     res.status(404).json(err.message)
   }
