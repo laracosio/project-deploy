f1400c4d2aebc2ab5b6ab390d5bc0c34d5c31232
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-10-22 21:02:07 +1100

    standardised layout for handler files to order of get > post > put > delete

--- src/handlers/otherHandler.ts
+++ src/handlers/otherHandler.ts
@@ -3,6 +3,13 @@ import { Router, Request, Response } from 'express';
 
 export const otherRouter = Router();
 
+// get routers
+
+// post routers
+
+// put routers
+
+// delete routers
 otherRouter.delete('/', (req: Request, res:Response) => {
   res.json(clear());
 });
