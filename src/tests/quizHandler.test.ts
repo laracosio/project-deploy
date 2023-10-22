453b550e6853e9e0e0d76b0da0bda6bbbb8ed557
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-10-22 23:09:19 +1100

    testing written for moveQuestion

--- src/tests/quizHandler.test.ts
+++ src/tests/quizHandler.test.ts
@@ -1,6 +1,7 @@
 import { authRegisterRequest, clearRequest, quizCreateRequest, quizRemoveRequest, quizInfoRequest, quizListRequest, quizNameUpdateRequest, quizDescriptUpdateRequest } from './serverTestHelper';
 import { person1, person2, person3, person4, person5, validQuizName, validQuizDescription, shortQuizName, invalidQuizName, longQuizName, longQuizDescription, newvalidQuizName, newvalidQuizDescription } from '../testingData';
 import { Response } from 'sync-request-curl';
+import { getUnixTime } from 'date-fns';
 
 beforeEach(() => {
   clearRequest();
@@ -492,5 +493,147 @@ describe('adminQuizDescriptionUpdate - Error Cases', () => {
     const quiz1Data = JSON.parse(quiz1.body.toString());
     const response = quizDescriptUpdateRequest(session1Data.token, quiz1Data.quizId, longQuizDescription);
     expect(response.statusCode).toStrictEqual(400);
+  });
+});
+
+describe('PUT /v1/admin/quiz/{quizid}/question/{questionid}/move - Success', () => {
+  let sess1: Response, quiz1: Response;
+  beforeEach(() => {
+    sess1 = authRegisterRequest(person1.email, person1.password, person1.nameFirst, person1.nameLast);
+    const sess1Data = JSON.parse(sess1.body.toString());
+    quiz1 = quizCreateRequest(sess1Data.token, validQuizName, validQuizDescription);
+  });
+  test('Move Question 2 to new Position and check lastEdited', () => {
+    const sess1Data = JSON.parse(sess1.body.toString());
+    const quiz1Data = JSON.parse(quiz1.body.toString());
+    // needs createQuestion
+    const quest1 = createQuestion(first question params);
+    const quest1Data = JSON.parse(quest1.body.toString());
+    const quest2 = createQuestion(second question params);
+    const quest2Data = JSON.parse(quest2.body.toString());
+    const res = moveQuestionRequest(sess1Data.token, quiz1Data.quizId, quest2Data.questionId, 0);
+    const data = JSON.parse(res.body.toString());
+    expect(data).toStrictEqual({});
+
+    const quizInfo = quizInfoRequest(sess1Data.token, quiz1Data.quizId);
+    expect(JSON.parse(quizInfo.body.toString()).timeLastEdited).toBeGreaterThanOrEqual(getUnixTime(new Date()));
+    // check hard coded params
+  });
+});
+
+describe('PUT /v1/admin/quiz/{quizid}/question/{questionid}/move - Error', () => {
+  let sess1: Response, quiz1: Response;
+  beforeEach(() => {
+    sess1 = authRegisterRequest(person1.email, person1.password, person1.nameFirst, person1.nameLast);
+    const sess1Data = JSON.parse(sess1.body.toString());
+    quiz1 = quizCreateRequest(sess1Data.token, validQuizName, validQuizDescription);
+  });
+  test('Quiz ID does not refer to a valid quiz', () => {
+    const sess1Data = JSON.parse(sess1.body.toString());
+    const quiz1Data = JSON.parse(quiz1.body.toString());
+    // needs createQuestion
+    const quest1 = createQuestion(first question params);
+    const quest1Data = JSON.parse(quest1.body.toString());
+    const quest2 = createQuestion(second question params);
+    const quest2Data = JSON.parse(quest2.body.toString());
+    const res = moveQuestionRequest(sess1Data.token, quiz1Data.quizId + 1, quest2Data.questionId, 0);
+    const data = JSON.parse(res.body.toString());
+    expect(data).toStrictEqual({ error: expect.any(String) });
+    expect(res.statusCode).toStrictEqual(400);
+  });
+  test('Question Id does not refer to a valid question within this quiz', () => {
+    const sess1Data = JSON.parse(sess1.body.toString());
+    const quiz1Data = JSON.parse(quiz1.body.toString());
+    // needs createQuestion
+    const quest1 = createQuestion(first question params);
+    const quest1Data = JSON.parse(quest1.body.toString());
+    const quest2 = createQuestion(second question params);
+    const quest2Data = JSON.parse(quest2.body.toString());
+    const res = moveQuestionRequest(sess1Data.token, quiz1Data.quizId, quest2Data.questionId + 1, 0);
+    const data = JSON.parse(res.body.toString());
+    expect(data).toStrictEqual({ error: expect.any(String) });
+    expect(res.statusCode).toStrictEqual(400);
+  });
+  test('NewPosition is less than 0', () => {
+    const sess1Data = JSON.parse(sess1.body.toString());
+    const quiz1Data = JSON.parse(quiz1.body.toString());
+    // needs createQuestion
+    const quest1 = createQuestion(first question params);
+    const quest1Data = JSON.parse(quest1.body.toString());
+    const quest2 = createQuestion(second question params);
+    const quest2Data = JSON.parse(quest2.body.toString());
+    const res = moveQuestionRequest(sess1Data.token, quiz1Data.quizId, quest2Data.questionId, -1);
+    const data = JSON.parse(res.body.toString());
+    expect(data).toStrictEqual({ error: expect.any(String) });
+    expect(res.statusCode).toStrictEqual(400);
+  });
+  test('NewPosition is greater than n-1 where n is the number of questions', () => {
+    const sess1Data = JSON.parse(sess1.body.toString());
+    const quiz1Data = JSON.parse(quiz1.body.toString());
+    // needs createQuestion
+    const quest1 = createQuestion(first question params);
+    const quest1Data = JSON.parse(quest1.body.toString());
+    const quest2 = createQuestion(second question params);
+    const quest2Data = JSON.parse(quest2.body.toString());
+    const res = moveQuestionRequest(sess1Data.token, quiz1Data.quizId, quest2Data.questionId, 3);
+    const data = JSON.parse(res.body.toString());
+    expect(data).toStrictEqual({ error: expect.any(String) });
+    expect(res.statusCode).toStrictEqual(400);
+  });
+  test('NewPosition is the position of the current question', () => {
+    const sess1Data = JSON.parse(sess1.body.toString());
+    const quiz1Data = JSON.parse(quiz1.body.toString());
+    // needs createQuestion
+    const quest1 = createQuestion(first question params);
+    const quest1Data = JSON.parse(quest1.body.toString());
+    const quest2 = createQuestion(second question params);
+    const quest2Data = JSON.parse(quest2.body.toString());
+    const res = moveQuestionRequest(sess1Data.token, quiz1Data.quizId, quest2Data.questionId, 1);
+    const data = JSON.parse(res.body.toString());
+    expect(data).toStrictEqual({ error: expect.any(String) });
+    expect(res.statusCode).toStrictEqual(400);
+  });
+  test('Token is empty', () => {
+    const sess1Data = JSON.parse(sess1.body.toString());
+    const quiz1Data = JSON.parse(quiz1.body.toString());
+    // needs createQuestion
+    const quest1 = createQuestion(first question params);
+    const quest1Data = JSON.parse(quest1.body.toString());
+    const quest2 = createQuestion(second question params);
+    const quest2Data = JSON.parse(quest2.body.toString());
+    const res = moveQuestionRequest('', quiz1Data.quizId, quest2Data.questionId, 0);
+    const data = JSON.parse(res.body.toString());
+    expect(data).toStrictEqual({ error: expect.any(String) });
+    expect(res.statusCode).toStrictEqual(401);
+  });
+  test('Token is invalid (does not refer to valid logged in user session)', () => {
+    const sess1Data = JSON.parse(sess1.body.toString());
+    const quiz1Data = JSON.parse(quiz1.body.toString());
+    // needs createQuestion
+    const quest1 = createQuestion(first question params);
+    const quest1Data = JSON.parse(quest1.body.toString());
+    const quest2 = createQuestion(second question params);
+    const quest2Data = JSON.parse(quest2.body.toString());
+    // needs log out session1
+    const res = moveQuestionRequest(sess1Data.token, quiz1Data.quizId, quest2Data.questionId, 0);
+    const data = JSON.parse(res.body.toString());
+    expect(data).toStrictEqual({ error: expect.any(String) });
+    expect(res.statusCode).toStrictEqual(401);
+  });
+  test('Valid token is provided, but user is not an owner of this quiz', () => {
+    const sess2 = authRegisterRequest(person2.email, person2.password, person2.nameFirst, person2.nameLast);
+    const sess2Data = JSON.parse(sess2.body.toString());
+    const sess1Data = JSON.parse(sess1.body.toString());
+    const quiz1Data = JSON.parse(quiz1.body.toString());
+    // needs createQuestion
+    const quest1 = createQuestion(first question params);
+    const quest1Data = JSON.parse(quest1.body.toString());
+    const quest2 = createQuestion(second question params);
+    const quest2Data = JSON.parse(quest2.body.toString());
+    // needs log out session1
+    const res = moveQuestionRequest(sess2Data.token, quiz1Data.quizId, quest2Data.questionId, 0);
+    const data = JSON.parse(res.body.toString());
+    expect(data).toStrictEqual({ error: expect.any(String) });
+    expect(res.statusCode).toStrictEqual(403);
   });
 });
