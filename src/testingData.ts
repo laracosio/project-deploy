39f0f0befaf13b54112fe35e02915607de83c9df
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-10-22 15:38:14 +1100

    added testingData for duplicate question

--- src/testingData.ts
+++ src/testingData.ts
@@ -1,3 +1,5 @@
+import { Question } from "./dataStore";
+
 const person1 = {
   email: 'h.simpson@springfield.com',
   password: 'quickend98',
@@ -61,6 +63,53 @@ const longQuizName = 'this is longer than thirty characters';
 const validQuizDescription = 'This quiz is awesome';
 const newvalidQuizDescription = 'This new quiz description is awesome';
 const longQuizDescription = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean moon.';
+const validQuestionInput1 = {
+  question: 'Who is the Monarch of England?',
+  duration: 4,
+  points: 5,
+  answers: [
+    {
+      answer: 'Prince Charles',
+      correct: true
+    },
+    {
+      answer: 'Shrek',
+      correct: false
+    },
+    {
+      answer: 'Prince Charming',
+      correct: false
+    },
+    {
+      answer: 'Rumpelstiltskin',
+      correct: false
+    }
+  ]
+}
+
+const validQuestionInput2 = {
+  question: 'Who ended the War of Roses?',
+  duration: 5,
+  points: 6,
+  answers: [
+    {
+      answer: 'Margaret Beaufort',
+      correct: false
+    },
+    {
+      answer: 'Thomas Cromwell',
+      correct: false
+    },
+    {
+      answer: 'Henry Tudor',
+      correct: true
+    },
+    {
+      answer: 'Richard Plantagenet',
+      correct: false
+    }
+  ]
+}
 
 export {
   person1,
@@ -78,4 +127,6 @@ export {
   validQuizDescription,
   newvalidQuizDescription,
   longQuizDescription,
+  validQuestionInput1,
+  validQuestionInput2
 };
