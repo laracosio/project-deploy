465cd4dd8a84cbb6bfd81ad8b1e44b03c502dc75
Joshua Beltran <z5488271@ad.unsw.edu.au>
2023-10-24 21:34:02 +1100

    Added implementation for quizViewTrash

--- src/features/trash.ts
+++ src/features/trash.ts
@@ -4,6 +4,15 @@ import { HttpStatusCode } from '../enums/HttpStatusCode';
 import { tokenValidation, findTokenUser } from './other';
 import { getUnixTime } from 'date-fns';
 
+interface BriefTrashQuizInfo {
+  quizId: number,
+  name: string
+}
+
+interface userTrashQuizList {
+  trash: BriefTrashQuizInfo[]
+}
+
 /**
  * Given a particular quiz, permanently remove the quiz.
  * @param {string} sessionId - unique token containing sessionId
@@ -37,4 +46,35 @@ function adminQuizRemove(sessionId: string, quizId: number): object {
   return {};
 }
 
-export { adminQuizRemove };
+/**
+ * Provide a list of all quizzes in the trash that are owned by the currently logged in user.
+ * @param {string} sessionId - unique token containing sessionId
+ * @returns {{error: string}}
+ */
+function quizViewTrash(sessionId: string, quizId: number): userTrashQuizList {
+  const dataStore = getData();
+
+  // check that sessionId is not empty or is valid
+  if (!tokenValidation(sessionId)) {
+    throw new ApiError('Invalid token', HttpStatusCode.UNAUTHORISED);
+  }
+
+  // find all user quizzes in the trash and add to an array
+  const tokenUser = findTokenUser(sessionId);
+  const userTrashQuizList: Array<BriefTrashQuizInfo> = [];
+  dataStore.trash.forEach((quiz) => {
+    if (quiz.quizOwner === tokenUser.userId) {
+      const obj = {
+        quizId: quiz.quizId,
+        name: quiz.name,
+      };
+      userTrashQuizList.push(obj);
+    }
+  });
+
+  return {
+    trash: userTrashQuizList
+  };
+}
+
+export { adminQuizRemove, quizViewTrash };
