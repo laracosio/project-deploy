371ed34867ee5f2b80a2d587716377e57400f112
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-10-24 19:59:57 +1100

    removed authRouter.details, router moved to userHandler

--- src/handlers/authHandler.ts
+++ src/handlers/authHandler.ts
@@ -5,11 +5,6 @@ import { Router, Request, Response } from 'express';
 export const authRouter = Router();
 
 // get Routers
-authRouter.get('/details', (req: Request, res: Response) => {
-  const token = req.query.token.toString();
-  const response = adminUserDetails(token);
-  res.send(response);
-});
 
 // post Routers
 authRouter.post('/register', (req: Request, res: Response) => {
