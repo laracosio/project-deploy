438595d9ea83c17ea124161722acf4a612a52978
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-11-04 08:08:15 +1100

    updated routes to V2

--- src/handlers/V2-handlers/quizHandlerV2.ts
+++ src/handlers/V2-handlers/quizHandlerV2.ts
@@ -11,35 +11,35 @@ export const quizRouterV2 = Router();
 
 // #region quiz post routers
 quizRouterV2.post('/:quizid/transfer', (req: Request, res: Response) => {
-    const token = req.header('token');
-    const userEmail = req.body.userEmail;
-    const quizId = parseInt(req.params.quizid);
-    res.json(adminQuizTransferOwner(token, quizId, userEmail));
+  const token = req.header('token');
+  const userEmail = req.body.userEmail;
+  const quizId = parseInt(req.params.quizid);
+  res.json(adminQuizTransferOwner(token, quizId, userEmail));
 });
 
 quizRouterV2.post('/:quizid/question/:questionid/duplicate', (req: Request, res: Response) => {
-    const quizId = parseInt(req.params.quizid);
-    const questionId = parseInt(req.params.questionid);
-    const token = req.header('token');
-    res.json(adminDuplicateQuestion(token, quizId, questionId));
+  const quizId = parseInt(req.params.quizid);
+  const questionId = parseInt(req.params.questionid);
+  const token = req.header('token');
+  res.json(adminDuplicateQuestion(token, quizId, questionId));
 });
 // #endregion
 
 // #region quiz put routers
 quizRouterV2.put('/:quizid/question/:questionid/move', (req: Request, res: Response) => {
-    const token = req.header('token');
-    const newPostion = req.body.newPosition as number;
-    const quizId = parseInt(req.params.quizid);
-    const questionId = parseInt(req.params.questionid);
-    res.json(adminMoveQuestion(token, quizId, questionId, newPostion));
+  const token = req.header('token');
+  const newPostion = req.body.newPosition as number;
+  const quizId = parseInt(req.params.quizid);
+  const questionId = parseInt(req.params.questionid);
+  res.json(adminMoveQuestion(token, quizId, questionId, newPostion));
 });
 // #endregion
 
 // #region quiz delete routers
 quizRouterV2.delete('/:quizid', (req: Request, res: Response) => {
-    const token = req.header('token');
-    const quizId = parseInt(req.params.quizid);
-    const response = adminQuizRemove(token, quizId);
-    res.json(response);
+  const token = req.header('token');
+  const quizId = parseInt(req.params.quizid);
+  const response = adminQuizRemove(token, quizId);
+  res.json(response);
 });
 // #endregion
