03421ff3db7adf584c166513ac50bb6320ee814a
Gina Kong <ginagkong@gmail.com>
2023-11-07 16:31:40 +1100

    Implemented POST /v2/admin/auth/logout server route

--- src/handlers/V2-handlers/authHandlerV2.ts
+++ src/handlers/V2-handlers/authHandlerV2.ts
@@ -1,10 +1,15 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+import { adminAuthLogout } from '../../features/auth';
 
 export const authRouterV2 = Router();
 
 // get Routers
 
 // post Routers
+authRouterV2.post('/logout', (req: Request, res: Response) => {
+  const token: string = req.header('token');
+  res.json(adminAuthLogout(token));
+})
 
 // put Routers
 
