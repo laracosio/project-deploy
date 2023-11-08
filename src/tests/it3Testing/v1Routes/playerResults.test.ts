673e3dca4c0533915778f9548db7ab33450e0bdb
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-11-08 16:00:52 +1100

    drafted player result and messaging tests

--- src/tests/it3Testing/v1Routes/playerResults.test.ts
+++ src/tests/it3Testing/v1Routes/playerResults.test.ts
@@ -2,3 +2,64 @@
 test('placeholder', () => {
   expect((1 + 1)).toBe(2);
 });
+
+/**
+ * 
+import { Response } from 'sync-request-curl';
+import { authRegisterRequest, clearRequest } from '../../it2Testing/serverTestHelperIt2';
+import { person1, validQuestionInput1, validQuestionInput2, validQuestionInput3, validQuizDescription, validQuizName } from '../../../testingData';
+import { createQuizQuestionRequestV2, quizCreateRequestV2 } from '../../serverTestHelperIt3';
+
+let session1: Response, quiz1: Response, player1: Response, player2: Response, player3: Response;
+beforeEach(() => {
+  clearRequest();
+  session1 = authRegisterRequest(person1.email, person1.password, person1.nameFirst, person1.nameLast);
+  const sess1Data = JSON.parse(session1.body.toString());
+  quiz1 = quizCreateRequestV2(sess1Data.token, validQuizName, validQuizDescription);
+  const quiz1Data = JSON.parse(quiz1.body.toString());
+  createQuizQuestionRequestV2(quiz1Data.quizId, sess1Data.token, validQuestionInput1);
+  createQuizQuestionRequestV2(quiz1Data.quizId, sess1Data.token, validQuestionInput2);
+  createQuizQuestionRequestV2(quiz1Data.quizId, sess1Data.token, validQuestionInput3);
+  // player 1 join
+  // player 2 join
+  // player 3 join
+  // start new session
+  // admin goes to update session to question countdown
+  // admin goes to skip countdown
+  // players submit answers - add timer
+  // admin changes to answer show state
+});
+
+// get results for a particular question of a session a player is playing in
+describe('GET /v1/player/:playerid/question/:questionposition/results - success', () => {
+  // success case - checking Q1
+  
+  // success case - checking Q3 (need admin to go to next Q)
+  // admin skips Q2
+  // skips countdown
+  // players submit answers - add timer
+  // admin changes to answerShow state
+})
+
+describe('GET /v1/player/:playerid/question/:questionposition/results - error', () => {
+  // invalid playerId
+  // try with invalid 4th player
+  // question position doesnt exist (< 0)
+  
+  // question position doesnt exist (exceeds #Questions)
+  // session is not in ANSWER_SHOW state
+  // session isnt on the input question
+})
+
+// Get the final results for a whole session a player is playing in
+describe('GET /v1/player/:playerid/results - success', () => {
+  // success case - check different outputs
+  // success case 
+})
+
+describe('GET /v1/player/:playerid/results - error', () => {
+  // invalid playerId
+  // not in final results stage
+})
+
+*/
