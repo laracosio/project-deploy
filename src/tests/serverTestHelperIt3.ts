43a4b815adce194eb5802da5624f0496aa6169ee
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-11-12 02:09:50 +1100

    added helper for playerQresult route call

--- src/tests/serverTestHelperIt3.ts
+++ src/tests/serverTestHelperIt3.ts
@@ -287,7 +287,12 @@ const quizInfoRequestV2 = (token: string, quizid: number): Response => {
 // #endregion
 
 // #region player handlers
-// place code here and delete this message
+const playerQuestResultRqst = (playerId: number, questionPosition: number): Response => {
+  return request(
+    'GET',
+    `${SERVER_URL}/v1/player/${playerId}/question/${questionPosition}/results`
+  );
+}
 // #endregion
 
 // #region session handlers
@@ -312,5 +317,6 @@ export {
   authUserDetailsRequestV2, quizRemoveRequestV2, quizTransferRequestV2, moveQuestionRequestV2, duplicateQuestionRequestV2,
   createQuizQuestionRequestV2, updateQuizQuestionRequestV2, deleteQuizQuestionRequestV2, quizViewTrashRequestV2, quizRestoreTrashRequestV2,
   quizEmptyTrashRequestV2, quizNameUpdateRequestV2, quizDescriptUpdateRequestV2, quizCreateRequestV2, quizListRequestV2,
-  quizInfoRequestV2, authLogoutRequestV2, userUpdateDetailsRequestV2, userUpdatePasswordRequestV2, sessionCreateRequest
+  quizInfoRequestV2, authLogoutRequestV2, userUpdateDetailsRequestV2, userUpdatePasswordRequestV2, sessionCreateRequest, 
+  playerQuestResultRqst
 };
