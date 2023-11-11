dd6d902a756c78083fda3546e3d2846037f498f5
Lara Michelle Cosio <z5403305@unsw.edu.au>
2023-11-11 16:32:19 +1100

    WIPs: Added handler and edited tests

--- src/tests/it3/v1Routes/guestPlayer.test.ts
+++ src/tests/it3/v1Routes/guestPlayer.test.ts
@@ -1,22 +1,31 @@
+import { authRegisterRequest, authLoginRequest } from "../../it2/serverTestHelperIt2";
+import { quizCreateRequestV2, createQuizQuestionRequestV2 } from "../../serverTestHelperIt3";
+import { person1, validQuizName, validQuizDescription, validCreateQuestionV2PNG } from "../../../testingData";
+import { joinGuestPlayerRequest, GuestPlayerStatusRequest } from "../../serverTestHelperIt3";
+import { HttpStatusCode } from "../../../enums/HttpStatusCode";
 // remove me and add your tests below
 test('placeholder', () => {
   expect((1 + 1)).toBe(2);
 });
 
 
-/*
+
 describe('Successful tests: Join Guest Player', () => {
   test('Join Guest Player: Valid name', () => {
     authRegisterRequest(person1.email, person1.password, person1.nameFirst, person1.nameLast);
     const personLogin = authLoginRequest(person1.email, person1.password);
     const personLoginParsed = JSON.parse(personLogin.body.toString());
-    const quizId = quizCreateRequest(personLoginParsed.token, validQuizName, validQuizDescription);
+    const quizId = quizCreateRequestV2(personLoginParsed.token, validQuizName, validQuizDescription);
     const quizIdParsed = JSON.parse(quizId.body.toString());
     const createQuizQuestion = createQuizQuestionRequestV2(quizIdParsed.quizId, personLoginParsed.token, validCreateQuestionV2PNG);
-  const sessionQuiz = startNewQuizSessionRequest(quizIdParsed, personLoginParsed, {autoStartNum: 3});
-  const sessionQuizParsed = JSON.parse(sessionQuiz.body.toString());
-  const name = "lara cosio";
-  const res = joinGuestPlayerRequest(sessionQuizParsed, name);
+
+    //CHANGE THIS
+    const sessionQuiz = startNewQuizSessionRequest(quizIdParsed, personLoginParsed, { autoStartNum: 3 });
+    const sessionQuizParsed = JSON.parse(sessionQuiz.body.toString());
+
+
+    const name = "lara cosio";
+    const res = joinGuestPlayerRequest(sessionQuizParsed, name);
     const data = JSON.parse(res.body.toString());
     expect(data).toStrictEqual({ playerId: expect.any(Number) });
     expect(res.statusCode).toBe(HttpStatusCode.OK);
@@ -25,13 +34,17 @@ describe('Successful tests: Join Guest Player', () => {
     authRegisterRequest(person1.email, person1.password, person1.nameFirst, person1.nameLast);
     const personLogin = authLoginRequest(person1.email, person1.password);
     const personLoginParsed = JSON.parse(personLogin.body.toString());
-    const quizId = quizCreateRequest(personLoginParsed.token, validQuizName, validQuizDescription);
+    const quizId = quizCreateRequestV2(personLoginParsed.token, validQuizName, validQuizDescription);
     const quizIdParsed = JSON.parse(quizId.body.toString());
     const createQuizQuestion = createQuizQuestionRequestV2(quizIdParsed.quizId, personLoginParsed.token, validCreateQuestionV2PNG);
-  const sessionQuiz = startNewQuizSessionRequest(quizIdParsed, personLoginParsed, {autoStartNum: 3});
-  const sessionQuizParsed = JSON.parse(sessionQuiz.body.toString());
-  const name = "";
-  const res = joinGuestPlayerRequest(sessionQuizParsed, name);
+    
+    //change this
+    const sessionQuiz = startNewQuizSessionRequest(quizIdParsed, personLoginParsed, { autoStartNum: 3 });
+    const sessionQuizParsed = JSON.parse(sessionQuiz.body.toString());
+    
+    
+    const name = "";
+    const res = joinGuestPlayerRequest(sessionQuizParsed, name);
     const data = JSON.parse(res.body.toString());
     expect(data).toStrictEqual({ playerId: expect.any(Number) });
     expect(res.statusCode).toBe(HttpStatusCode.OK);
@@ -44,30 +57,35 @@ describe('Unsuccessful tests: Join Guest Player', () => {
     authRegisterRequest(person1.email, person1.password, person1.nameFirst, person1.nameLast);
     const personLogin = authLoginRequest(person1.email, person1.password);
     const personLoginParsed = JSON.parse(personLogin.body.toString());
-    const quizId = quizCreateRequest(personLoginParsed.token, validQuizName, validQuizDescription);
+    const quizId = quizCreateRequestV2(personLoginParsed.token, validQuizName, validQuizDescription);
     const quizIdParsed = JSON.parse(quizId.body.toString());
-    const createQuizQuestion = createQuizQuestionRequestV2(quizIdParsed.quizId, personLoginParsed.token, validCreateQuestionV2PNG);
-  const sessionQuiz = startNewQuizSessionRequest(quizIdParsed, personLoginParsed, {autoStartNum: 3});
-  const sessionQuizParsed = JSON.parse(sessionQuiz.body.toString());
-  const name = "lara cosio";
-  const first = joinGuestPlayerRequest(sessionQuizParsed, name);
-      const res = joinGuestPlayerRequest(sessionQuizParsed, name);
+    createQuizQuestionRequestV2(quizIdParsed.quizId, personLoginParsed.token, validCreateQuestionV2PNG);
+    
+    //changer this
+    const sessionQuiz = startNewQuizSessionRequest(quizIdParsed, personLoginParsed, { autoStartNum: 3 });
+    const sessionQuizParsed = JSON.parse(sessionQuiz.body.toString());
+    
+    const name = "lara michelle";
+    const first = joinGuestPlayerRequest(sessionQuizParsed, name);
+    const res = joinGuestPlayerRequest(sessionQuizParsed, name);
     const data = JSON.parse(res.body.toString());
     expect(data).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(HttpStatusCode.BAD_REQUEST);
   });
   test('Unsuccessful: Session is not in LOBBY state', () => {
-TODO!
     authRegisterRequest(person1.email, person1.password, person1.nameFirst, person1.nameLast);
     const personLogin = authLoginRequest(person1.email, person1.password);
     const personLoginParsed = JSON.parse(personLogin.body.toString());
-    const quizId = quizCreateRequest(personLoginParsed.token, validQuizName, validQuizDescription);
+    const quizId = quizCreateRequestV2(personLoginParsed.token, validQuizName, validQuizDescription);
     const quizIdParsed = JSON.parse(quizId.body.toString());
-    const createQuizQuestion = createQuizQuestionRequestV2(quizIdParsed.quizId, personLoginParsed.token, validCreateQuestionV2PNG);
-  const sessionQuiz = startNewQuizSessionRequest(quizIdParsed, personLoginParsed, {autoStartNum: 3});
-  const sessionQuizParsed = JSON.parse(sessionQuiz.body.toString());
-  const name = "lara";
-  const res = joinGuestPlayerRequest(sessionQuizParsed, name);
+    createQuizQuestionRequestV2(quizIdParsed.quizId, personLoginParsed.token, validCreateQuestionV2PNG);
+    
+    //change this
+    const sessionQuiz = startNewQuizSessionRequest(quizIdParsed, personLoginParsed, { autoStartNum: 3 });
+    const sessionQuizParsed = JSON.parse(sessionQuiz.body.toString());
+    
+    const name = "lara michelle";
+    const res = joinGuestPlayerRequest(sessionQuizParsed, name);
     const data = JSON.parse(res.body.toString());
     expect(data).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(HttpStatusCode.OK);
@@ -79,21 +97,26 @@ describe('Successful tests: Status of Guest Player', () => {
     authRegisterRequest(person1.email, person1.password, person1.nameFirst, person1.nameLast);
     const personLogin = authLoginRequest(person1.email, person1.password);
     const personLoginParsed = JSON.parse(personLogin.body.toString());
-    const quizId = quizCreateRequest(personLoginParsed.token, validQuizName, validQuizDescription);
+    const quizId = quizCreateRequestV2(personLoginParsed.token, validQuizName, validQuizDescription);
     const quizIdParsed = JSON.parse(quizId.body.toString());
-    const createQuizQuestion = createQuizQuestionRequestV2(quizIdParsed.quizId, personLoginParsed.token, validCreateQuestionV2PNG);
-  const sessionQuiz = startNewQuizSessionRequest(quizIdParsed, personLoginParsed, {autoStartNum: 3});
-  const sessionQuizParsed = JSON.parse(sessionQuiz.body.toString());
-  const name = "lara cosio";
-  const joinGuestPlayer = joinGuestPlayerRequest(sessionQuizParsed, name);
-    Const joinGuestPlayerParsed = JSON.parse(joinGuestPlayer.body.toString);
-    const res = GuestPlayerStatusRequest(joinGuestPlayerParsed.playerId)
+    createQuizQuestionRequestV2(quizIdParsed.quizId, personLoginParsed.token, validCreateQuestionV2PNG);
+    
+    //change this
+    const sessionQuiz = startNewQuizSessionRequest(quizIdParsed, personLoginParsed, { autoStartNum: 3 });
+    const sessionQuizParsed = JSON.parse(sessionQuiz.body.toString());
+    
+    const name = "lara cosio";
+    const joinGuestPlayer = joinGuestPlayerRequest(sessionQuizParsed, name);
+    const joinGuestPlayerParsed = JSON.parse(joinGuestPlayer.body.toString());
+
+    //mock here
+    const res = GuestPlayerStatusRequest(joinGuestPlayerParsed.playerId);
     const data = JSON.parse(res.body.toString());
     expect(data).toStrictEqual({
-  "state": "LOBBY",
-  "numQuestions": expect.any(Number),
-  "atQuestion": expect.any(Number) 
-});
+      "state": "LOBBY",
+      "numQuestions": expect.any(Number),
+      "atQuestion": expect.any(Number)
+    });
     expect(res.statusCode).toBe(HttpStatusCode.OK);
   });
 });
@@ -103,19 +126,25 @@ describe('Unsuccessful tests: Status of Guest Player', () => {
     authRegisterRequest(person1.email, person1.password, person1.nameFirst, person1.nameLast);
     const personLogin = authLoginRequest(person1.email, person1.password);
     const personLoginParsed = JSON.parse(personLogin.body.toString());
-    const quizId = quizCreateRequest(personLoginParsed.token, validQuizName, validQuizDescription);
+    const quizId = quizCreateRequestV2(personLoginParsed.token, validQuizName, validQuizDescription);
     const quizIdParsed = JSON.parse(quizId.body.toString());
-    const createQuizQuestion = createQuizQuestionRequestV2(quizIdParsed.quizId, personLoginParsed.token, validCreateQuestionV2PNG);
-	const sessionQuiz = startNewQuizSessionRequest(quizIdParsed, personLoginParsed, {autoStartNum: 3});
-	const sessionQuizParsed = JSON.parse(sessionQuiz.body.toString());
-	const name = "lara cosio";
-	const joinGuestPlayer = joinGuestPlayerRequest(sessionQuizParsed, name);
-    Const joinGuestPlayerParsed = JSON.parse(joinGuestPlayer.body.toString);
-    const res = GuestPlayerStatusRequest(joinGuestPlayerParsed.playerId+10)
+    createQuizQuestionRequestV2(quizIdParsed.quizId, personLoginParsed.token, validCreateQuestionV2PNG);
+    
+    //change this
+    const sessionQuiz = startNewQuizSessionRequest(quizIdParsed, personLoginParsed, { autoStartNum: 3 });
+    const sessionQuizParsed = JSON.parse(sessionQuiz.body.toString());
+    
+    const name = "lara cosio";
+    
+    const joinGuestPlayer = joinGuestPlayerRequest(sessionQuizParsed, name);
+    const joinGuestPlayerParsed = JSON.parse(joinGuestPlayer.body.toString());
+
+    //mock here
+
+    const res = GuestPlayerStatusRequest(joinGuestPlayerParsed.playerId + 10);
     const data = JSON.parse(res.body.toString());
     expect(data).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(HttpStatusCode.BAD_REQUEST);
   });
 });
 
-*/
