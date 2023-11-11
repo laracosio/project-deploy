130ce1df3a10fe2659a319b3e857e709d8ef1ca2
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-11-12 02:03:36 +1100

    added playerHandler for playerQuestionResult

--- src/handlers/v1/playerHandlerV1.ts
+++ src/handlers/v1/playerHandlerV1.ts
@@ -1,3 +1,18 @@
-import { Router } from 'express';
+
+import { Router, Request, Response } from 'express';
+import { playerQuestionResult } from '../../services/playerService';
 
 export const playerRouter = Router();
+
+// get routers
+playerRouter.get('/:playerid/question/:questionposition/results', (req: Request, res: Response) => {
+    const playerId = parseInt(req.params.playerid);
+    const questionPosition = parseInt(req.params.questionposition);
+    res.json(playerQuestionResult(playerId, questionPosition));
+})
+
+// post routers
+
+// put routers
+
+// delete routers
