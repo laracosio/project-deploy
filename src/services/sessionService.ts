c4a7269c4d5bfb91f9b8c401e77edc05043cd1e2
Joshua Beltran <z5488271@ad.unsw.edu.au>
2023-11-15 19:35:02 +1100

    Removed console logs

--- src/services/sessionService.ts
+++ src/services/sessionService.ts
@@ -213,8 +213,6 @@ export function updateState(session: Session, action: AdminActions | AutomaticAc
   });
 
   let quizSessionsList: quizSessionsList = {activeSessions: quizActiveSessionList, inactiveSessions: quizInactiveSessionList};
-
-  console.log(quizSessionsList);
-  
+    
   return quizSessionsList
 }
