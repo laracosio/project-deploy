caa7b30d9e6291446bcc7c856f328dd3895f511b
Joshua Beltran <z5488271@ad.unsw.edu.au>
2023-11-15 21:09:39 +1100

    fixed linting

--- src/tests/it3/v1Routes/quizThumbnail.test.ts
+++ src/tests/it3/v1Routes/quizThumbnail.test.ts
@@ -1,4 +1,4 @@
-import { clearRequest, authRegisterRequest, quizCreateRequest, quizNameUpdateRequest } from '../../it2/serverTestHelperIt2';
+import { clearRequest, authRegisterRequest, quizCreateRequest } from '../../it2/serverTestHelperIt2';
 import { person1, person2, validQuizDescription, validQuizName, validpngUrl1, validpngUrl2, validjpgUrl1, validjpgUrl2, invalidimgUrl, unfetchableimgUrl } from '../../../testingData';
 import { quizThumbnailUpdateRequest } from '../../serverTestHelperIt3';
 import { Response } from 'sync-request-curl';
