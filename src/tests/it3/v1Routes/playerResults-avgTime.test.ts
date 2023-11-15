ba3d4682b2f34682ce9b6fe476a2cd60dd78b756
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-11-16 10:59:34 +1100

    changed session to user

--- src/tests/it3/v1Routes/playerResults-avgTime.test.ts
+++ src/tests/it3/v1Routes/playerResults-avgTime.test.ts
@@ -10,14 +10,14 @@ import { HttpStatusCode } from '../../../enums/HttpStatusCode';
 test('long multiple choice test to calculate avgTime', async() => {
   clearRequest();
   // create admin
-  const session1 = authRegisterRequest(person1.email, person1.password, person1.nameFirst, person1.nameLast);
-  const sess1Data = JSON.parse(session1.body.toString());
+  const user1 = authRegisterRequest(person1.email, person1.password, person1.nameFirst, person1.nameLast);
+  const user1Data = JSON.parse(user1.body.toString());
   // create quiz and add 3 questions
-  const quiz1 = quizCreateRequestV2(sess1Data.token, validQuizName, validQuizDescription);
+  const quiz1 = quizCreateRequestV2(user1Data.token, validQuizName, validQuizDescription);
   const quiz1Data = JSON.parse(quiz1.body.toString());
-  createQuizQuestionRequestV2(quiz1Data.quizId, sess1Data.token, longMultiChoice);
+  createQuizQuestionRequestV2(quiz1Data.quizId, user1Data.token, longMultiChoice);
   // create session and add 3 players
-  const game1 = sessionCreateRequest(sess1Data.token, quiz1Data.quizId, validAutoStartNum);
+  const game1 = sessionCreateRequest(user1Data.token, quiz1Data.quizId, validAutoStartNum);
   const game1Data = JSON.parse(game1.body.toString());
   const player1 = joinGuestPlayerRequest(game1Data.sessionId, 'Gizmo');
   const player2 = joinGuestPlayerRequest(game1Data.sessionId, 'Dave');
@@ -26,11 +26,11 @@ test('long multiple choice test to calculate avgTime', async() => {
   const player2Data = JSON.parse(player2.body.toString());
   const player3Data = JSON.parse(player3.body.toString());
   // start game and by-pass countdown
-  updateSessionRequest(sess1Data.token, quiz1Data.quizId, game1Data.sessionId, AdminActions.NEXT_QUESTION);
-  updateSessionRequest(sess1Data.token, quiz1Data.quizId, game1Data.sessionId, AdminActions.SKIP_COUNTDOWN);
+  updateSessionRequest(user1Data.token, quiz1Data.quizId, game1Data.sessionId, AdminActions.NEXT_QUESTION);
+  updateSessionRequest(user1Data.token, quiz1Data.quizId, game1Data.sessionId, AdminActions.SKIP_COUNTDOWN);
 
   // quizInfo to get answerIds
-  const quizInfo = quizInfoRequestV2(sess1Data.token, quiz1Data.quizId);
+  const quizInfo = quizInfoRequestV2(user1Data.token, quiz1Data.quizId);
   const quizInfoData = JSON.parse(quizInfo.body.toString());
   const question1 = quizInfoData.questions[0];
   // players submit answers to Q1
@@ -46,7 +46,7 @@ test('long multiple choice test to calculate avgTime', async() => {
   playerSubmitAnswerRequest(player3Data.playerId, 1, numArray);
   // question is closed and changes to ANSWER_SHOW
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  updateSessionRequest(sess1Data.token, quiz1Data.quizId, game1Data.sessionId, AdminActions.GO_TO_ANSWER);
+  updateSessionRequest(user1Data.token, quiz1Data.quizId, game1Data.sessionId, AdminActions.GO_TO_ANSWER);
   const res = playerQuestResultRqst(player1Data.playerId, 1);
 
   const data = JSON.parse(res.body.toString());
