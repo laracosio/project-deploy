86da08fe775c822b56e180a617b762bc3eb91c84
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-11-16 19:17:23 +1100

    removed duplicate get path

--- src/handlers/v1/sessionHandlerV1.ts
+++ src/handlers/v1/sessionHandlerV1.ts
@@ -2,24 +2,17 @@ import { Router, Request, Response } from 'express';
 import { startNewSession, getSessionStatus, viewSessions, updateSessionStatus } from '../../services/sessionService';
 export const sessionRouterV1 = Router();
 
-sessionRouterV1.get('/:quizId/session/:sessionId', (req: Request, res: Response) => {
-  const quizId: number = parseInt(req.params.quizId);
-  const sessionId: number = parseInt(req.params.sessionId);
-  const token: string = req.header('token');
-  res.json(getSessionStatus(quizId, sessionId, token));
-});
-
-sessionRouterV1.get('/:quizId/session/:sessionId', (req: Request, res: Response) => {
-  const quizId: number = parseInt(req.params.quizId);
-  const sessionId: number = parseInt(req.params.sessionId);
-  const token: string = req.header('token');
-  res.json(getSessionStatus(quizId, sessionId, token));
-});
-
 sessionRouterV1.get('/:quizId/sessions', (req: Request, res: Response) => {
   const token: string = req.header('token');
   const quizId: number = parseInt(req.params.quizId);
   res.json(viewSessions(token, quizId));
+});
+
+sessionRouterV1.get('/:quizId/session/:sessionId', (req: Request, res: Response) => {
+  const quizId: number = parseInt(req.params.quizId);
+  const sessionId: number = parseInt(req.params.sessionId);
+  const token: string = req.header('token');
+  res.json(getSessionStatus(quizId, sessionId, token));
 });
 
 sessionRouterV1.put('/:quizId/session/:sessionId', (req: Request, res: Response) => {
