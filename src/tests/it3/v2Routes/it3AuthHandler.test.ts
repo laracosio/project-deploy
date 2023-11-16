46b4d523499bc06ae7ec0d590fd409f7ba7db8cf
Carmen Zhang <z3415901@ad.unsw.edu.au>
2023-11-16 11:17:39 +1100

    changed testingData for playerResults, need to change expected values in test

--- src/tests/it3/v2Routes/it3AuthHandler.test.ts
+++ src/tests/it3/v2Routes/it3AuthHandler.test.ts
@@ -11,46 +11,46 @@ beforeEach(() => {
 
 // auth/logout tests
 describe('POST /v2/admin/auth/logout - Success Cases', () => {
-  let session1: Response, session2: Response, session3: Response;
+  let user1: Response, user2: Response, user3: Response;
   beforeEach(() => {
-    session1 = authRegisterRequest(person1.email, person1.password, person1.nameFirst, person1.nameLast);
-    session2 = authLoginRequest(person1.email, person1.password);
-    session3 = authLoginRequest(person1.email, person1.password);
+    user1 = authRegisterRequest(person1.email, person1.password, person1.nameFirst, person1.nameLast);
+    user2 = authLoginRequest(person1.email, person1.password);
+    user3 = authLoginRequest(person1.email, person1.password);
   });
   test('Token was successfully removed from dataStore - single logout from multiple sessions for same user', () => {
-    const session1Data = JSON.parse(session1.body.toString());
-    const session2Data = JSON.parse(session2.body.toString());
-    const session3Data = JSON.parse(session3.body.toString());
+    const user1Data = JSON.parse(user1.body.toString());
+    const user2Data = JSON.parse(user2.body.toString());
+    const user3Data = JSON.parse(user3.body.toString());
 
-    const response: Response = authLogoutRequestV2(session3Data.token);
+    const response: Response = authLogoutRequestV2(user3Data.token);
     expect(response.statusCode).toStrictEqual(200);
     expect(JSON.parse(response.body.toString())).toStrictEqual({});
 
     if (fs.existsSync('datastore.json')) {
       const datastr: Buffer = fs.readFileSync('./datastore.json');
       const data: Datastore = JSON.parse(String(datastr));
-      expect(data.mapUT.some(user => user.token === session1Data.token)).toStrictEqual(true);
-      expect(data.mapUT.some(user => user.token === session2Data.token)).toStrictEqual(true);
-      expect(data.mapUT.some(user => user.token === session3Data.token)).toStrictEqual(false);
+      expect(data.mapUT.some(user => user.token === user1Data.token)).toStrictEqual(true);
+      expect(data.mapUT.some(user => user.token === user2Data.token)).toStrictEqual(true);
+      expect(data.mapUT.some(user => user.token === user3Data.token)).toStrictEqual(false);
     }
   });
   test('Token was successfully removed from dataStore - multiple logouts from multiple sessions for same user', () => {
-    const session1Data = JSON.parse(session1.body.toString());
-    const session2Data = JSON.parse(session2.body.toString());
-    const session3Data = JSON.parse(session3.body.toString());
+    const user1Data = JSON.parse(user1.body.toString());
+    const user2Data = JSON.parse(user2.body.toString());
+    const user3Data = JSON.parse(user3.body.toString());
 
-    authLogoutRequestV2(session1Data.token);
-    authLogoutRequestV2(session2Data.token);
-    const response: Response = authLogoutRequestV2(session3Data.token);
+    authLogoutRequestV2(user1Data.token);
+    authLogoutRequestV2(user2Data.token);
+    const response: Response = authLogoutRequestV2(user3Data.token);
     expect(response.statusCode).toStrictEqual(200);
     expect(JSON.parse(response.body.toString())).toStrictEqual({});
 
     if (fs.existsSync('datastore.json')) {
       const datastr: Buffer = fs.readFileSync('./datastore.json');
       const data: Datastore = JSON.parse(String(datastr));
-      expect(data.mapUT.some(user => user.token === session1Data.token)).toStrictEqual(false);
-      expect(data.mapUT.some(user => user.token === session2Data.token)).toStrictEqual(false);
-      expect(data.mapUT.some(user => user.token === session3Data.token)).toStrictEqual(false);
+      expect(data.mapUT.some(user => user.token === user1Data.token)).toStrictEqual(false);
+      expect(data.mapUT.some(user => user.token === user2Data.token)).toStrictEqual(false);
+      expect(data.mapUT.some(user => user.token === user3Data.token)).toStrictEqual(false);
     }
   });
 });
